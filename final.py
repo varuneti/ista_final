6b7e40165e83ae5a69dfd8464a68451b8701eb89
Varun Neti <varunneti@Varuns-MacBook-Pro.local>
2023-04-30 20:57:12 -0700

    Committing ALL figures

--- final.py
+++ final.py
@@ -57,5 +57,31 @@ def figure2():
     matplotlib.pyplot.title("Weekly COVID-19 Cases by Year", fontsize = 18)
     matplotlib.pyplot.show()
 
+def figure3(): 
+    cases_df = get_cases()
+    cases_df['Date'] = pd.to_datetime(cases_df['Date'], format='%b %d %Y')
+    mask = (cases_df['Date'] >= pd.Timestamp('2020-03-01')) & (cases_df['Date'] <= pd.Timestamp('2021-03-01'))
+    filtered_cases_df = cases_df.loc[mask]
+    dates_list = filtered_cases_df['Date'].tolist()
+    cases_list = filtered_cases_df['Weekly Cases'].tolist()
+
+    df = pd.DataFrame({'date': dates_list, 'cases': cases_list})
+    df['date'] = pd.to_datetime(df['date'], format='%b %d %Y')
+    dates = df['date'].values
+    cases = df['cases'].values
+    slope, intercept = np.polyfit(matplotlib.dates.date2num(dates), cases, 1)
+    line = slope * matplotlib.dates.date2num(dates) + intercept
+    fig, ax = matplotlib.pyplot.subplots(figsize=(12,7))
+    ax.set_ylim([(cases.min()), (cases.max()) + 100000])
+    ax.scatter(dates, cases)
+    ax.plot(dates, matplotlib.dates.num2date(line), color='red')
+    matplotlib.pyplot.ylabel("Weekly Cases(In Hundred Thousandths)", fontsize = 12)
+    matplotlib.pyplot.xlabel("Months", fontsize = 12)
+    matplotlib.pyplot.title("Weekly COVID-19 Cases From Mar 2020 to Mar 2021", fontsize = 18)
+    ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%b %Y'))
+    matplotlib.pyplot.show()
+
 
 figure1()
+figure2()
+figure3()
